736ec45638121af56aed22eee5d35047d8df92f0
kenil <kenilsoni2710@gmail.com>
2026-09-01 11:42:03 +0530

    Day 1

---
+++ test.py
@@ -0,0 +1,10 @@
+from config import GEMINI_KEY
+
+# def genrate():
+#     print("Hello World!")
+#     return "Calling the function genrate()"
+# response = genrate()
+# full_response = "Here is the response: ok " + str(response)
+# print(full_response)
+
+print("GEMINI_KEY:", GEMINI_KEY)    
